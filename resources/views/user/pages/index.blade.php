cd59d55678e6b8e9dc93444305b21b703c57c9e5
iqbaliamal <iqbaliamal@gmail.com>
2021-07-10 18:17:09 +0700

    penerima manfaat

--- resources/views/user/pages/index.blade.php
+++ resources/views/user/pages/index.blade.php
@@ -49,7 +49,7 @@
                             <div class="progress">
                                 <div class="progress-bar" role="progressbar" style="width: 90%" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
-                            <p class="card-text deadline"><b>{{ moneyFormat($campaign->total) }}</b> terkumpul dari
+                            <p class="card-text deadline"><b>{{ moneyFormat($danaSementara) }}</b> terkumpul dari
                                 <b>{{ moneyFormat($campaign->target_donation) }}</b> </p>
                             <div class="deadline">
                                 @if (\Carbon\Carbon::parse( $campaign->max_date )->diffInDays( Carbon\Carbon::now()) >
@@ -75,24 +75,65 @@
 
     <!-- Section Penerima Manfaat -->
     <section id="penerima">
+        <div class="section-header">
+            <div class="container">
+            <h2>Penerima Manfaat</h2>
+        </div>
+        </div>
+        <div class="section-count">
         <div class="container">
-            <div class="section-header">
-                <h2>Penerima Manfaat</h2>
-            </div>
 
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="icon-box">
-                        <h3>Rp 100.000.000</h3>
-                        <p>Menerima Donasi</p>
-
+            <div class="row ">
+                <div class="col-md-3 col-sm-6 col-12">
+                    <div class="card shadow border-0 p-5 recap-list">
+                        <span class="recap-list-icon">
+                            <img src="{{asset('user/img/icon/donasi.svg')}}" alt="">
+                        </span>
+                        <span class="recap-list-detail">
+                            <h1 id="ododonasi" class="odometer odometer-auto-theme mb-1">843</h1>
+                            <p>Penerima Manfaat</p>
+                        </span>
                     </div>
                 </div>
-                <div class="col-lg-3">alskdj</div>
-                <div class="col-lg-3">alskdj</div>
-                <div class="col-lg-3">alskdj</div>
+                <div class="col-md-3 col-sm-6 col-12">
+                    <div class="card shadow border-0 p-5 recap-list">
+                        <span class="recap-list-icon">
+                            <img src="{{asset('user/img/icon/relawan.svg')}}" alt="">
+                        </span>
+                        <span class="recap-list-detail">
+
+                            <h1 id="odorelawan" class="odometer odometer-auto-theme mb-1">923</h1>
+                            <p>Total Donasi</p>
+                        </span>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-12">
+                    <div class="card shadow border-0 p-5 recap-list">
+                        <span class="recap-list-icon">
+                            <img src="{{asset('user/img/icon/donatur.svg')}}" alt="">
+                        </span>
+                        <span class="recap-list-detail">
+
+                            <h1 id="ododonatur" class="odometer odometer-auto-theme mb-1">18</h1>
+                            <p>Program Kebaikan</p>
+                        </span>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-12">
+                    <div class="card shadow border-0 p-5 recap-list">
+                        <span class="recap-list-icon">
+                            <img src="{{asset('user/img/icon/kolab.svg')}}" alt="">
+                        </span>
+                        <span class="recap-list-detail">
+
+                            <h1 id="odokolaborator" class="odometer odometer-auto-theme mb-1">8723</h1>
+                            <p>Total Relawan</p>
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
     </section><!-- End Peneima Section -->
 
     <!-- Section Program Kebaikan -->
@@ -106,7 +147,7 @@
                 @foreach ($programs as $program)
                 <div class="col-lg-4">
                     <div class="box wow fadeInLeft">
-                        <div class="icon"><i class="fa fa-bar-chart"></i></div>
+                        <div class="icon shadow-sm"><img src="{{$program->image}}" style="max-width: 75px;" alt=""></div>
                         <h4 class="title"><a href="">{{$program->title}}</a></h4>
                         <p class="description">{{$program->description}}</p>
                     </div>
