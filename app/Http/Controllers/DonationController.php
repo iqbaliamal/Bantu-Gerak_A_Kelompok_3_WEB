03ddee5ec7d39e86597aa314b33dc3b8900b5455
mitawafawulandari16 <71862877+mitawafawulandari16@users.noreply.github.com>
2021-07-16 21:07:39 +0700

    validasi donasi

--- app/Http/Controllers/DonationController.php
+++ app/Http/Controllers/DonationController.php
@@ -27,8 +27,13 @@ class DonationController extends Controller
     {
         $this->validate($request, [
             'date_from'  => 'required',
-            'date_to'    => 'required',
-        ]);
+            'date_to'    => 'required'
+        ],
+    [
+        'date_from.required'=>'tanggal donasi donatur wajib diisi',
+        'date_to.required'=>'waktu penggunaan wajib diisi ',
+    ]
+);
 
         $date_from  = $request->date_from;
         $date_to    = $request->date_to;
