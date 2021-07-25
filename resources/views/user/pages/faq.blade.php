d913226ddb6414bd3bb8230c872f27160529674f
ensasviga <flampard210@gmail.com>
2021-07-25 15:43:53 +0700

    faq

---
+++ resources/views/user/pages/faq.blade.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<section class="section-4" id="faq">
+        <div class="container faq">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="faq-title">
+                        <h1 class="title-1">Frequently Answered Question</h1>
+                    </div>
+                    <div class="accordion accordion-flush" id="accordionFlushExample" data-aos="zoom-in-up">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingOne">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseOne" aria-expanded="false"
+                                    aria-controls="flush-collapseOne">
+                                    Apakah ada batasan usia untuk peserta yang mengikuti lomba Bisnis TIK?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseOne" class="accordion-collapse collapse"
+                                aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Tidak ada. Sejauh peserta terdaftar sebagai mahasiswa dibuktikan dari kartu mahasiswa yang masih aktif.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingTwo">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseTwo" aria-expanded="false"
+                                    aria-controls="flush-collapseTwo">
+                                    Apakah satu tim boleh mengirimkan lebih dari 1 proposal Bisnis TIK?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseTwo" class="accordion-collapse collapse"
+                                aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Boleh, Namun untuk pembayaran registrasi tetap berlaku per proposal. Yaitu Rp. 100.000 untuk Mahasiswa Umum dan Rp. 70.000 untuk Mahasiswa Politeknik Negeri Jember
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingThree">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseThree" aria-expanded="false"
+                                    aria-controls="flush-collapseThree">
+                                    Apakah boleh mengirim karya yang pernah menang di lomba lain?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseThree" class="accordion-collapse collapse"
+                                aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Tidak. Karya yang dikirimkan harus merupakan karya yang belum pernah menang di ajang perlombaan apapun.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingFour">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseFour" aria-expanded="false"
+                                    aria-controls="flush-collapseFour">
+                                    Jika kami sebagai tim (2 / 3 orang), apakah semuanya harus mendaftar?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseFour" class="accordion-collapse collapse"
+                                aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Untuk tim, cukup ketua yang mendaftar, lalu isikan data ketua dan anggota dengan lengkap dan benar.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingFive">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseFive" aria-expanded="false"
+                                    aria-controls="flush-collapseFive">
+                                    Jika kami tidak memiliki dosen pembimbing, apakah tidak masalah bila dikosongkan?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseFive" class="accordion-collapse collapse"
+                                aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Setiap tim harus memiliki dosen pembimbing dan tidak boleh dikosongkan
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingSix">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseSix" aria-expanded="false"
+                                    aria-controls="flush-collapseSix">
+                                    Apakah seluruh peserta mendapatkan sertifikat peserta ?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseSix" class="accordion-collapse collapse"
+                                aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Iya, mendapatkan, seluruh peserta yang terdaftar akan mendapatkan e-sertifikat individu
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingSeven">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseSeven" aria-expanded="false"
+                                    aria-controls="flush-collapseSeven">
+                                    Apakah Mahasiswa Luar Politeknik Negeri Jember dapat mendaftar sebagai perserta Expo/Pameran?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseSeven" class="accordion-collapse collapse"
+                                aria-labelledby="flush-headingSeven" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Tidak, Peserta Expo Produk IT hanya berasal dari mahasiswa Jurusan Teknologi Informasi Politeknik Negeri Jember
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingEight">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseEight" aria-expanded="false"
+                                    aria-controls="flush-collapseEight">
+                                    Bagaimana cara mengunjungi Expo secara Online?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseEight" class="accordion-collapse collapse"
+                                aria-labelledby="flush-headingEight" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Dengan cara mengunjungi tautan <a href="http://expo.epimjti.com" target="_blank">http://expo.epimjti.com/</a>. atau dengan cara klik kategori pada navigasi bar website ini.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingNine">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseNine" aria-expanded="false"
+                                    aria-controls="flush-collapseNine">
+                                    Apakah ada batasan usia untuk peserta yang mengikuti lomba?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseNine" class="accordion-collapse collapse"
+                                aria-labelledby="flush-headingNine" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Tidak ada. Sejauh peserta terdaftar sebagai siswa SMK dibuktikan dari kartu tanda pelajar  yang masih aktif.
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingTen">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseTen" aria-expanded="false"
+                                    aria-controls="flush-collapseTen">
+                                    Apakah seluruh peserta mendapatkan sertifikat peserta ?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseTen" class="accordion-collapse collapse"
+                                aria-labelledby="flush-headingTen" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Iya mendapatkan, semua peserta mendapatkan sertifikat sebagai peserta
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingEleven">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseEleven" aria-expanded="false"
+                                    aria-controls="flush-collapseEleven">
+                                    Bagaimana jika kartu tanda pelajar hilang/tidak ada ?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseEleven" class="accordion-collapse collapse"
+                                aria-labelledby="flush-headingEleven" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Kartu tanda pelajar yang hilang/tidak ada, dapat digantikan dengan surat keterangan aktif dari sekolah.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingTwelve">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseTwelve" aria-expanded="false"
+                                    aria-controls="flush-collapseTwelve">
+                                    Apakah biaya pendaftaran peserta dapat digabung dengan siswa/peserta yang satu sekolah?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseTwelve" class="accordion-collapse collapse"
+                                aria-labelledby="flush-headingTwelve" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Bisa, jika peserta berasal dari satu sekolah yang sama.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingThirdTeen">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseThirdTeen" aria-expanded="false"
+                                    aria-controls="flush-collapseThirdTeen">
+                                    Apakah ada batasan pendaftar perwakilan sekolah?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseThirdTeen" class="accordion-collapse collapse"
+                                aria-labelledby="flush-headingThirdTeen" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">Tidak ada batasan peserta, meskipun dari satu sekolah yang sama.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
